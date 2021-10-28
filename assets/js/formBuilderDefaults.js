0de31ed4793e94f4011267d0bf73721c7b99dfa6
Seth Pletcher <seth.pletcher@gmail.com>
2021-10-27 21:04:21 -0400

    finalized schema for cases collection

--- assets/js/formBuilderDefaults.js
+++ assets/js/formBuilderDefaults.js
@@ -147,282 +147,14 @@ class FormBuilderDefaults {
         }
     ];
 
-    /*
-    static DEFAULT_CASE_SCHEMA = {
-        "caseNumber": 0,
-        "userId": "w4sGxWrab8h8a2yn9",
-        "dateCreated": new Date(),
-        "lastUpdated": new Date(),
-        "general": [
-            {
-                "type": "text",
-                "required": false,
-                "label": "Client Name",
-                "placeholder": "Meddling P.I. LLC",
-                "className": "form-control",
-                "name": "clientName"
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Investigation Type",
-                "className": "form-control",
-                "name": "investigationType"
-            },
-            {
-                "type": "date",
-                "required": false,
-                "label": "Start Date",
-                "className": "form-control",
-                "name": "startDate"
-            },
-            {
-                "type": "checkbox-group",
-                "required": false,
-                "label": "Case Status",
-                "toggle": true,
-                "name": "caseStatus",
-                "values": [
-                    {
-                        "label": "Open/Closed",
-                        "value": "open",
-                        "selected": true
-                    }
-                ]
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Tasking",
-                "placeholder": "The objective of this case is to...",
-                "className": "form-control",
-                "name": "tasking"
-            },
-            {
-                "type": "textarea",
-                "required": false,
-                "label": "Conclusions &amp; Recommendations",
-                "className": "form-control",
-                "name": "conclusions"
-            }
-        ],
-        "subjects": [
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Name",
-                "placeholder" : "John Doe",
-                "className" : "form-control",
-                "name" : "name"
-            },
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Description",
-                "placeholder" : "Subject 1",
-                "className" : "form-control",
-                "name" : "description"
-            },
-            {
-                "type" : "textarea",
-                "required" : false,
-                "label" : "Notes",
-                "placeholder" : "Additional notes go here...",
-                "className" : "form-control",
-                "name" : "notes"
-            }
-        ],
-        "addresses": [
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Address",
-                "placeholder" : "123 Main St., Plainfield, VT, 05301",
-                "className" : "form-control",
-                "name" : "address"
-            },
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Description",
-                "placeholder" : "Home of Subject",
-                "className" : "form-control",
-                "name" : "description"
-            },
-            {
-                "type" : "textarea",
-                "required" : false,
-                "label" : "Notes",
-                "placeholder" : "Additional notes go here...",
-                "className" : "form-control",
-                "name" : "notes"
-            }
-        ],
-        "vehicles": [
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Vehicle",
-                "placeholder" : "1999 / VW Jetta / Green / ABC123",
-                "className" : "form-control",
-                "name" : "vehicle"
-            },
-            {
-                "type" : "text",
-                "required" : false,
-                "label" : "Description",
-                "placeholder" : "Vehicle of Subject",
-                "className" : "form-control",
-                "name" : "description"
-            },
-            {
-                "type" : "textarea",
-                "required" : false,
-                "label" : "Notes",
-                "placeholder" : "Additional notes go here...",
-                "className" : "form-control",
-                "name" : "notes"
-            }
-        ]
-    };
-     */
 
-    // TODO incorporate into form builder
-    static DEFAULT_FORM_INPUTS = {
-        "general": [
-            {
-                "type": "text",
-                "required": false,
-                "label": "Client Name",
-                "placeholder": "Meddling P.I. LLC",
-                "className": "form-control",
-                "name": "clientName"
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Investigation Type",
-                "className": "form-control",
-                "name": "investigationType"
-            },
-            {
-                "type": "date",
-                "required": false,
-                "label": "Start Date",
-                "className": "form-control",
-                "name": "startDate"
-            },
-            {
-                "type": "checkbox-group",
-                "required": false,
-                "label": "Case Status",
-                "toggle": true,
-                "name": "caseStatus",
-                "values": [
-                    {
-                        "label": "Open/Closed",
-                        "value": "open",
-                        "selected": true
-                    }
-                ]
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Tasking",
-                "placeholder": "The objective of this case is to...",
-                "className": "form-control",
-                "name": "tasking"
-            },
-            {
-                "type": "textarea",
-                "required": false,
-                "label": "Conclusions &amp; Recommendations",
-                "className": "form-control",
-                "name": "conclusions"
-            }
-        ],
-        "subjects": [
-            {
-                "type": "text",
-                "required": false,
-                "label": "Name",
-                "placeholder": "John Doe",
-                "className": "form-control",
-                "name": "name"
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Description",
-                "placeholder": "Subject 1",
-                "className": "form-control",
-                "name": "description"
-            },
-            {
-                "type": "textarea",
-                "required": false,
-                "label": "Notes",
-                "placeholder": "Additional notes go here...",
-                "className": "form-control",
-                "name": "notes"
-            }
-        ],
-        "addresses": [
-            {
-                "type": "text",
-                "required": false,
-                "label": "Address",
-                "placeholder": "123 Main St., Plainfield, VT, 05301",
-                "className": "form-control",
-                "name": "address"
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Description",
-                "placeholder": "Home of Subject",
-                "className": "form-control",
-                "name": "description"
-            },
-            {
-                "type": "textarea",
-                "required": false,
-                "label": "Notes",
-                "placeholder": "Additional notes go here...",
-                "className": "form-control",
-                "name": "notes"
-            }
-        ],
-        "vehicles": [
-            {
-                "type": "text",
-                "required": false,
-                "label": "Vehicle",
-                "placeholder": "1999 / VW Jetta / Green / ABC123",
-                "className": "form-control",
-                "name": "vehicle"
-            },
-            {
-                "type": "text",
-                "required": false,
-                "label": "Description",
-                "placeholder": "Vehicle of Subject",
-                "className": "form-control",
-                "name": "description"
-            },
-            {
-                "type": "textarea",
-                "required": false,
-                "label": "Notes",
-                "placeholder": "Additional notes go here...",
-                "className": "form-control",
-                "name": "notes"
-            }
-        ]
-    };
-
-    static GENERAL_DEFAULT_SCHEMA = [
+/*
+static DEFAULT_CASE_SCHEMA = {
+    "caseNumber": 0,
+    "userId": "w4sGxWrab8h8a2yn9",
+    "dateCreated": new Date(),
+    "lastUpdated": new Date(),
+    "general": [
         {
             "type": "text",
             "required": false,
@@ -474,9 +206,145 @@ class FormBuilderDefaults {
             "className": "form-control",
             "name": "conclusions"
         }
-    ];
+    ],
+    "subjects": [
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Name",
+            "placeholder" : "John Doe",
+            "className" : "form-control",
+            "name" : "name"
+        },
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Description",
+            "placeholder" : "Subject 1",
+            "className" : "form-control",
+            "name" : "description"
+        },
+        {
+            "type" : "textarea",
+            "required" : false,
+            "label" : "Notes",
+            "placeholder" : "Additional notes go here...",
+            "className" : "form-control",
+            "name" : "notes"
+        }
+    ],
+    "addresses": [
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Address",
+            "placeholder" : "123 Main St., Plainfield, VT, 05301",
+            "className" : "form-control",
+            "name" : "address"
+        },
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Description",
+            "placeholder" : "Home of Subject",
+            "className" : "form-control",
+            "name" : "description"
+        },
+        {
+            "type" : "textarea",
+            "required" : false,
+            "label" : "Notes",
+            "placeholder" : "Additional notes go here...",
+            "className" : "form-control",
+            "name" : "notes"
+        }
+    ],
+    "vehicles": [
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Vehicle",
+            "placeholder" : "1999 / VW Jetta / Green / ABC123",
+            "className" : "form-control",
+            "name" : "vehicle"
+        },
+        {
+            "type" : "text",
+            "required" : false,
+            "label" : "Description",
+            "placeholder" : "Vehicle of Subject",
+            "className" : "form-control",
+            "name" : "description"
+        },
+        {
+            "type" : "textarea",
+            "required" : false,
+            "label" : "Notes",
+            "placeholder" : "Additional notes go here...",
+            "className" : "form-control",
+            "name" : "notes"
+        }
+    ]
+};
+ */
 
-    static SUBJECTS_DEFAULT_SCHEMA = [
+// TODO incorporate into form builder
+static
+DEFAULT_FORM_INPUTS = {
+    "general": [
+        {
+            "type": "text",
+            "required": false,
+            "label": "Client Name",
+            "placeholder": "Meddling P.I. LLC",
+            "className": "form-control",
+            "name": "clientName"
+        },
+        {
+            "type": "text",
+            "required": false,
+            "label": "Investigation Type",
+            "className": "form-control",
+            "name": "investigationType"
+        },
+        {
+            "type": "date",
+            "required": false,
+            "label": "Start Date",
+            "className": "form-control",
+            "name": "startDate"
+        },
+        {
+            "type": "checkbox-group",
+            "required": false,
+            "label": "Case Status",
+            "toggle": true,
+            "name": "caseStatus",
+            "values": [
+                {
+                    "label": "Open/Closed",
+                    "value": "open",
+                    "selected": true
+                }
+            ]
+        },
+        {
+            "type": "text",
+            "required": false,
+            "label": "Tasking",
+            "placeholder": "The objective of this case is to...",
+            "className": "form-control",
+            "name": "tasking"
+        },
+        {
+            "type": "textarea",
+            "required": false,
+            "label": "Conclusions &amp; Recommendations",
+            "className": "form-control",
+            "name": "conclusions"
+        }
+    ],
+    "subjects": [
         {
             "type": "text",
             "required": false,
@@ -501,9 +369,8 @@ class FormBuilderDefaults {
             "className": "form-control",
             "name": "notes"
         }
-    ];
-
-    static ADDRESSES_DEFAULT_SCHEMA = [
+    ],
+    "addresses": [
         {
             "type": "text",
             "required": false,
@@ -528,9 +395,8 @@ class FormBuilderDefaults {
             "className": "form-control",
             "name": "notes"
         }
-    ];
-
-    static VEHICLES_DEFAULT_SCHEMA = [
+    ],
+    "vehicles": [
         {
             "type": "text",
             "required": false,
@@ -555,5 +421,145 @@ class FormBuilderDefaults {
             "className": "form-control",
             "name": "notes"
         }
-    ];
+    ]
+};
+
+static
+GENERAL_DEFAULT_SCHEMA = [
+    {
+        "type": "text",
+        "required": false,
+        "label": "Client Name",
+        "placeholder": "Meddling P.I. LLC",
+        "className": "form-control",
+        "name": "clientName"
+    },
+    {
+        "type": "text",
+        "required": false,
+        "label": "Investigation Type",
+        "className": "form-control",
+        "name": "investigationType"
+    },
+    {
+        "type": "date",
+        "required": false,
+        "label": "Start Date",
+        "className": "form-control",
+        "name": "startDate"
+    },
+    {
+        "type": "checkbox-group",
+        "required": false,
+        "label": "Case Status",
+        "toggle": true,
+        "name": "caseStatus",
+        "values": [
+            {
+                "label": "Open/Closed",
+                "value": "open",
+                "selected": true
+            }
+        ]
+    },
+    {
+        "type": "text",
+        "required": false,
+        "label": "Tasking",
+        "placeholder": "The objective of this case is to...",
+        "className": "form-control",
+        "name": "tasking"
+    },
+    {
+        "type": "textarea",
+        "required": false,
+        "label": "Conclusions &amp; Recommendations",
+        "className": "form-control",
+        "name": "conclusions"
+    }
+];
+
+static
+SUBJECTS_DEFAULT_SCHEMA = [
+    {
+        "type": "text",
+        "required": false,
+        "label": "Name",
+        "placeholder": "John Doe",
+        "className": "form-control",
+        "name": "name"
+    },
+    {
+        "type": "text",
+        "required": false,
+        "label": "Description",
+        "placeholder": "Subject 1",
+        "className": "form-control",
+        "name": "description"
+    },
+    {
+        "type": "textarea",
+        "required": false,
+        "label": "Notes",
+        "placeholder": "Additional notes go here...",
+        "className": "form-control",
+        "name": "notes"
+    }
+];
+
+static
+ADDRESSES_DEFAULT_SCHEMA = [
+    {
+        "type": "text",
+        "required": false,
+        "label": "Address",
+        "placeholder": "123 Main St., Plainfield, VT, 05301",
+        "className": "form-control",
+        "name": "address"
+    },
+    {
+        "type": "text",
+        "required": false,
+        "label": "Description",
+        "placeholder": "Home of Subject",
+        "className": "form-control",
+        "name": "description"
+    },
+    {
+        "type": "textarea",
+        "required": false,
+        "label": "Notes",
+        "placeholder": "Additional notes go here...",
+        "className": "form-control",
+        "name": "notes"
+    }
+];
+
+static
+VEHICLES_DEFAULT_SCHEMA = [
+    {
+        "type": "text",
+        "required": false,
+        "label": "Vehicle",
+        "placeholder": "1999 / VW Jetta / Green / ABC123",
+        "className": "form-control",
+        "name": "vehicle"
+    },
+    {
+        "type": "text",
+        "required": false,
+        "label": "Description",
+        "placeholder": "Vehicle of Subject",
+        "className": "form-control",
+        "name": "description"
+    },
+    {
+        "type": "textarea",
+        "required": false,
+        "label": "Notes",
+        "placeholder": "Additional notes go here...",
+        "className": "form-control",
+        "name": "notes"
+    }
+];
 }
